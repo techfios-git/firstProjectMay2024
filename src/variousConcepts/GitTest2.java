072bf982822ac344e09f458632c2a00447ee9f8e
techfios-git <shihabul.kh@gmail.com>
2024-11-10 10:28:40 -0600

    added new code

---
+++ src/variousConcepts/GitTest2.java
@@ -0,0 +1,27 @@
+package variousConcepts;
+
+public class GitTest2 {
+	
+	public static void main(String[] args) {
+		
+		gitTest1();
+		gitTest2();
+		gitTest3();
+		
+	}
+
+	public static void gitTest1() {
+		
+		System.out.println("Test 1");
+	}
+	
+	public static void gitTest2() {
+		
+		System.out.println("Test 3");
+	}
+	
+	public static void gitTest3() {
+		
+		System.out.println("Test 3");
+	}
+}
